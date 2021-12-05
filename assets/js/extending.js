5107dbddc29dc9b796e9d73e1d66fed4715ab7fe
John D'Orazio <priest@johnromanodorazio.com>
2021-12-05 14:24:40 +0100

    remove unnecessary img tag

--- assets/js/extending.js
+++ assets/js/extending.js
@@ -913,7 +913,6 @@ $(document).on('click', '#deleteDiocesanCalendarButton', function(){
             console.log('data returned from delete action: ');
             console.log(data);
             toastr["success"]("Diocesan Calendar was deleted successfully", "Success");
-            $('<div class="toast" role="alert" aria-live="assertive" aria-atomic="true"><div class="toast-header"><img src="..." class="rounded mr-2" alt="..."><strong class="mr-auto">Bootstrap</strong><small class="text-muted">just now</small><button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="toast-body">See? Just like this.</div></div>').toast('show');
         },
         error: function (xhr, textStatus, errorThrown) {
             console.log(xhr.status + ' ' + textStatus + ': ' + errorThrown);
