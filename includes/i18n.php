0705f61aeb9141786b6e5eda648923fe9a9fe181
John D'Orazio <priest@johnromanodorazio.com>
2021-12-19 16:39:00 +0100

    cleanup last remnants of PHP translation arrays

--- includes/i18n.php
+++ includes/i18n.php
@@ -6,133 +6,9 @@ error_reporting(E_ALL);
 
 class i18n {
 
-    const MESSAGES = [
-        "Usage" => [
-            "de" => "Verwendung",
-            "en" => "Usage",
-            "es" => "Uso",
-            "fr" => "Usage",
-            "it" => "Utilizzo",
-            "pt" => "Uso"
-        ],
-        "Extending the API" => [
-            "de" => "API erweitern",
-            "en" => "Extending the API",
-            "es" => "Ampliando la API",
-            "fr" => "Extension de l'API",
-            "it" => "Estendere l'API",
-            "pt" => "Extensão da API"
-        ],
-        "About us" => [
-            "de" => "Wer wir sind",
-            "en" => "About us",
-            "es" => "Quienes somos",
-            "fr" => "Qui nous sommes",
-            "it" => "Chi siamo",
-            "pt" => "Quem nós somos"
-        ],
-        "Create a Diocesan Calendar" => [
-            "de" => "Erstellen Sie einen Diözesankalender",
-            "en" => "Create a Diocesan Calendar",
-            "es" => "Crea un calendario diocesano",
-            "fr" => "Créer un calendrier diocésain",
-            "it" => "Crea un calendario diocesano",
-            "pt" => "Crie um calendário diocesano"
-        ],
-        "Create a National Calendar" => [
-            "de" => "Erstellen Sie einen nationalen Kalender",
-            "en" => "Create a National Calendar",
-            "es" => "Crea un calendario nacional",
-            "fr" => "Créer un calendrier national",
-            "it" => "Crea un calendario nazionale",
-            "pt" => "Crie um calendário nacional"
-        ],
-        "Month" => [
-            "de" => "Monat",
-            "en" => "Month",
-            "es" => "Mes",
-            "fr" => "Mois",
-            "it" => "Mese",
-            "lat" => "Mensis",
-            "pt" => "Mês"
-        ],
-        "Day" => [
-            "de" => "Tag",
-            "en" => "Day",
-            "es" => "Día",
-            "fr" => "Jour",
-            "it" => "Giorno",
-            "lat" => "Dies",
-            "pt" => "Dia"
-        ],
-        "Name" => [
-            "de" => "Name",
-            "en" => "Name",
-            "es" => "Nombre",
-            "fr" => "Nom",
-            "it" => "Nome",
-            "lat" => "Nomen",
-            "pt" => "Nome"
-        ],
-        "Liturgical color" => [
-            "de" => "Liturgische Farbe",
-            "en" => "Liturgical color",
-            "es" => "Color litúrgico",
-            "fr" => "Couleur liturgique",
-            "it" => "Colore liturgico",
-            "lat" => "Color liturgicum",
-            "pt" => "Cor litúrgica"
-        ],
-        "Solemnities" => [
-            "de" => "Feierlichkeiten",
-            "en" => "Solemnities",
-            "es" => "Solemnidades",
-            "fr" => "Solennités",
-            "it" => "Solennità",
-            "lat" => "Sollemnitates",
-            "pt" => "Solenidades"
-        ],
-        "Feasts" => [
-            "de" => "Feste",
-            "en" => "Feasts",
-            "es" => "Fiestas",
-            "fr" => "Fêtes",
-            "it" => "Feste",
-            "lat" => "Festuum",
-            "pt" => "Festas"
-        ],
-        "Memorials" => [
-            "de" => "Gedenkfeiern",
-            "en" => "Memorials",
-            "es" => "Memorias",
-            "fr" => "Mémoires",
-            "it" => "Memorie obbligatorie",
-            "lat" => "Memoriae",
-            "pt" => "Memórias"
-        ],
-        "Optional memorials" => [
-            "de" => "Optionale Gedenkfeiers",
-            "en" => "Optional memorials",
-            "es" => "Memorias opcionales",
-            "fr" => "Mémoires optionnelles",
-            "it" => "Memorie facoltative",
-            "lat" => "Memoriae ad libitum",
-            "pt" => "Memórias opcionais"
-        ],
-        "Since" => [
-            "en" => "Since",
-            "it" => "Dall'anno",
-            "lat" => "Ab anno"
-        ]
-    ];
-
-    
-
-    public array $messages;
-
     public string $LOCALE;
 
-    public function __construct( array $messages = null ) {
+    public function __construct() {
 
         if( !empty( $_COOKIE["currentLocale"] ) ) {
             $this->LOCALE = $_COOKIE["currentLocale"];
@@ -160,9 +36,6 @@ class i18n {
         bindtextdomain("litcal", "i18n");
         textdomain("litcal");
 
-        if( $messages !== null ) {
-            $this->messages = array_merge( self::MESSAGES, $messages );
-        }
     }
 
 
--- usage.php
+++ usage.php
@@ -2,37 +2,7 @@
 
 include_once("includes/i18n.php");
 
-/**
- * Define our translation strings for this page
-*/
-$messages = [
-    "General Roman Calendar" => [
-        "de" => "Allgemeiner Römischer Kalender",
-        "en" => "General Roman Calendar",
-        "es" => "Calendario Romano General",
-        "fr" => "Calendrier Général Romain",
-        "it" => "Calendario Romano Generale",
-        "pt" => "Calendário Romano Geral",
-    ],
-    "Catholic Liturgical Calendar" => [
-        "de" => "Katholischer Liturgischer Kalender",
-        "en" => "Catholic Liturgical Calendar",
-        "es" => "Calendario Litúrgico Católico",
-        "fr" => "Calendrier Liturgique Catholique",
-        "it" => "Calendario Liturgico Cattolico",
-        "pt" => "Calendário Litúrgico Católico",
-    ],
-    "API_DESCRIPTION" => [
-        "de" => "Eine API für den liturgischen Kalender, aus der Sie Daten für die liturgischen Ereignisse eines bestimmten Jahres ab 1970 abrufen können, sei es für den universellen oder allgemeinen römischen Kalender oder für abgeleitete nationale und diözesane Kalender",
-        "en" => "A Liturgical Calendar API from which you can retrieve data for the Liturgical events of any given year from 1970 onwards, whether for the Universal or General Roman Calendar or for derived National and Diocesan calendars",
-        "es" => "Una API para el Calendario Litúrgico del cual puede recuperar datos para los eventos litúrgicos de cualquier año desde 1970 en adelante, ya sea para el Calendario Romano General o Universal o para los calendarios nacionales y diocesanos derivados.",
-        "fr" => "Une API pour le calendrier liturgique à partir de laquelle vous pouvez récupérer des données pour les événements liturgiques d'une année donnée à partir de 1970, que ce soit pour le calendrier romain universel ou général ou pour les calendriers nationaux et diocésains dérivés",
-        "it" => "Una API per il Calendario Liturgico, da cui estrarre i dati degli eventi liturgici di un qualsiasi anno dal 1970 in poi, sia per il Calendario Romano Universale che per i calendari nazionali e diocesani derivati",
-        "pt" => "Uma API para o calendário litúrgico do qual você pode recuperar dados para os eventos litúrgicos de qualquer ano a partir de 1970, seja para o calendário romano universal ou geral ou para calendários nacionais e diocesanos derivados"
-    ]
-];
-
-$i18n = new i18n( $messages );
+$i18n = new i18n();
 
 $API_DESCRIPTION = _( "A Liturgical Calendar API from which you can retrieve data for the Liturgical events of any given year from 1970 onwards, whether for the Universal or General Roman Calendar or for derived National and Diocesan calendars" );
 
@@ -41,7 +11,7 @@ $API_DESCRIPTION = _( "A Liturgical Calendar API from which you can retrieve dat
 <!doctype html>
 <html lang="<?php echo $i18n->LOCALE; ?>">
 <head>
-    <title><?php echo _( "General Roman Calendar") . ' - Examples ' ?></title>
+    <title><?php echo _( "General Roman Calendar") . ' - ' . _( 'Examples' ) ?></title>
     <?php include_once('./layout/head.php'); ?>
 </head>
 <body>
