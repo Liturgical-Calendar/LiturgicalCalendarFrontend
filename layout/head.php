cdbdec6f71602f8de87d2f488128274fb3e9f783
John D'Orazio <priest@johnromanodorazio.com>
2023-02-27 19:57:25 +0100

    fix toastr on usage page

--- layout/head.php
+++ layout/head.php
@@ -3,14 +3,14 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="icon" type="image/x-icon" href="favicon.ico">
 <meta name="msapplication-TileColor" content="#ffffff"/>
-<meta name="msapplication-TileImage" content="easter-egg-5-144-279148.png">
+<meta name="msapplication-TileImage" content="assets/img/easter-egg-5-144-279148.png">
 <link rel="apple-touch-icon-precomposed" sizes="152x152" href="assets/img/easter-egg-5-152-279148.png">
 <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/img/easter-egg-5-144-279148.png">
 <link rel="apple-touch-icon-precomposed" sizes="120x120" href="assets/img/easter-egg-5-120-279148.png">
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/img/easter-egg-5-114-279148.png">
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/img/easter-egg-5-72-279148.png">
-<link rel="apple-touch-icon-precomposed" href="assets/imgeaster-egg-5-57-279148.png">
-<link rel="icon" href="img/easter-egg-5-32-279148.png" sizes="32x32">
+<link rel="apple-touch-icon-precomposed" href="assets/img/easter-egg-5-57-279148.png">
+<link rel="icon" href="assets/img/easter-egg-5-32-279148.png" sizes="32x32">
 
 <!-- Custom fonts for this template-->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" rel="stylesheet" type="text/css">
@@ -28,7 +28,7 @@
     if($pageName === "index"){
         echo '<link href="assets/css/homepage.css">';
     }
-    if( $pageName === 'extending' || $pageName === 'admin' ) {
+    if( in_array( $pageName, [ 'extending', 'usage', 'admin' ] ) ) {
         echo '<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/1.1.1/css/bootstrap-multiselect.min.css" rel="stylesheet">';
         echo '<link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">';
     }
