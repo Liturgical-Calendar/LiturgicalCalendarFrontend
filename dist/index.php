db526ff03f6da3fc46704ada3130511959f56ff5
John D'Orazio <priest@johnromanodorazio.com>
2023-07-05 08:29:16 +0200

    use remote index.css for swagger ui

--- dist/index.php
+++ dist/index.php
@@ -1,5 +1,5 @@
 <?php 
-const SWAGGER_UI_DIST_VERSION = '4.15.5';
+const SWAGGER_UI_DIST_VERSION = '5.1.0';
 ?>
 <!-- HTML for static distribution bundle build -->
 <!DOCTYPE html>
@@ -8,29 +8,9 @@ const SWAGGER_UI_DIST_VERSION = '4.15.5';
     <meta charset="UTF-8">
     <title>Swagger UI</title>
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@<?php echo SWAGGER_UI_DIST_VERSION; ?>/swagger-ui.css" >
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@<?php echo SWAGGER_UI_DIST_VERSION; ?>/index.css" >
     <link rel="icon" type="image/png" href="https://unpkg.com/swagger-ui-dist@<?php echo SWAGGER_UI_DIST_VERSION; ?>/favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="https://unpkg.com/swagger-ui-dist@<?php echo SWAGGER_UI_DIST_VERSION; ?>/favicon-16x16.png" sizes="16x16" />
-    <style>
-      html
-      {
-        box-sizing: border-box;
-        overflow: -moz-scrollbars-vertical;
-        overflow-y: scroll;
-      }
-
-      *,
-      *:before,
-      *:after
-      {
-        box-sizing: inherit;
-      }
-
-      body
-      {
-        margin:0;
-        background: #fafafa;
-      }
-    </style>
   </head>
 
   <body>
@@ -40,7 +20,6 @@ const SWAGGER_UI_DIST_VERSION = '4.15.5';
     <script src="https://unpkg.com/swagger-ui-dist@<?php echo SWAGGER_UI_DIST_VERSION; ?>/swagger-ui-standalone-preset.js" crossorigin> </script>
     <script>
     window.onload = function() {
-      
       // Begin Swagger UI call region
       const ui = SwaggerUIBundle({
         url: "https://raw.githubusercontent.com/JohnRDOrazio/LiturgicalCalendar/master/schemas/openapi.json",
@@ -55,10 +34,7 @@ const SWAGGER_UI_DIST_VERSION = '4.15.5';
         ],
         layout: "StandaloneLayout",
       })
-      
-      
       // End Swagger UI call region
-
 
       window.ui = ui
     }
