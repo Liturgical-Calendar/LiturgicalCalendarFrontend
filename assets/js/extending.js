d777eb9309cd7f801ee56201ff53f0462b284cac
John R. D'Orazio <priest@johnromanodorazio.com>
2024-10-03 04:59:27 +0200

    debug Object entries AvailableLocalesWithRegion

--- assets/js/extending.js
+++ assets/js/extending.js
@@ -51,7 +51,7 @@ const { LOCALE, LOCALE_WITH_REGION, AvailableLocalesWithRegion, AvailableCountri
 const jsLocale = LOCALE.replace('_', '-');
 FormControls.jsLocale = jsLocale;
 FormControls.weekdayFormatter = new Intl.DateTimeFormat(jsLocale, { weekday: "long" });
-console.log(AvailableLocalesWithRegion);
+console.log(Object.entries(AvailableLocalesWithRegion));
 
 /**
  * Proxy sanitizer for the proxied API object. Sanitizes the values assigned to properties of the proxied API object.
