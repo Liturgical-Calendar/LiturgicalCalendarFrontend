05872d17bad7ff4b945dbeb55a64e549bd52d30d
John R. D'Orazio <priest@johnromanodorazio.com>
2025-01-08 02:23:20 +0300

    implement new PathBuilder component

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,83 +1,10 @@
-import { ApiClient, CalendarSelect, ApiOptions, Input, ApiOptionsFilter } from '@liturgical-calendar/components-js';
+import { ApiClient, CalendarSelect, ApiOptions, Input, ApiOptionsFilter, PathBuilder } from '@liturgical-calendar/components-js';
 
 Input.setGlobalInputClass('form-select');
 Input.setGlobalLabelClass('form-label mb-1');
 Input.setGlobalWrapper('div');
 Input.setGlobalWrapperClass('form-group col col-md-3');
 
-/**
- * CalendarCategory
- * @typedef {('nation'|'diocese')} CalendarCategory
- */
-
-/**
- * CalendarType
- * @readonly
- * @enum {CalendarCategory} Used in building the endpoint URL for requests to the API /calendar endpoint
- */
-const CalendarType = {
-    NATIONAL: 'nation',
-    DIOCESAN: 'diocese'
-}
-Object.freeze(CalendarType);
-
-
-/**
- * Describes the URL parameters that can be set on the API /calendar endpoint
- * @module RequestPayload
- */
-class RequestPayload {
-    /** @type {?Epiphany} - Whether Epiphany is to be celebrated on January 6 or on the Sunday between January 2 and January 8 */
-    static epiphany             = null;
-    /** @type {?Ascension} - Whether Ascension is to be celebrated on Thursday or on Sunday */
-    static ascension            = null;
-    /** @type {?CorpusChristi} - Whether Corpus Christi is to be celebrated on Thursday or on Sunday */
-    static corpus_christi       = null;
-    /** @type {?EternalHighPriest} - Whether Eternal High Priest is to be celebrated */
-    static eternal_high_priest  = null;
-    /** @type {?Locale} - The locale in which the liturgical calendar should be produced */
-    static locale               = null;
-    /** @type {?ReturnType} - The format of the response data */
-    static return_type          = null;
-    /** @type {?YearType} - Whether the liturgical calendar data should be for the liturgical year or the civil year */
-    static year_type            = null;
-};
-
-
-/**
- * Used to build the full endpoint URL for the API /calendar endpoint
- * @module CurrentEndpoint
- *
- */
-class CurrentEndpoint {
-    /**
-     * The base URL of the API /calendar endpoint
-     * @returns {string} The base URL of the API /calendar endpoint
-     */
-    static get apiBase() {
-        return `${CalendarURL}`
-    };
-    static calendarType   = null;
-    static calendarId     = null;
-    static calendarYear   = null;
-    static serialize = () => {
-        let currentEndpoint = CurrentEndpoint.apiBase;
-        if ( CurrentEndpoint.calendarType !== null && CurrentEndpoint.calendarId !== null ) {
-            currentEndpoint += `/${CurrentEndpoint.calendarType}/${CurrentEndpoint.calendarId}`;
-        }
-        if ( CurrentEndpoint.calendarYear !== null ) {
-            currentEndpoint += `/${CurrentEndpoint.calendarYear}`;
-        }
-        let parameters = [];
-        for (const key in RequestPayload) {
-            if(RequestPayload[key] !== null && RequestPayload[key] !== ''){
-                parameters.push(key + "=" + encodeURIComponent(RequestPayload[key]));
-            }
-        }
-        let urlParams = parameters.length ? `?${parameters.join('&')}` : '';
-        return `${currentEndpoint}${urlParams}`;
-    }
-}
 
 ApiClient.init().then(apiClient => {
     if (false === apiClient || false === apiClient instanceof ApiClient) {
@@ -111,111 +38,23 @@ ApiClient.init().then(apiClient => {
         apiOptions.filter( ApiOptionsFilter.ALL_PATHS ).appendTo('#requestParametersAllPaths');
         apiOptions.linkToCalendarSelect( calendarSelect );
 
-        /*
-        apiClient.listenTo( calendarSelect );
-        apiClient.listenTo( apiOptions );
-        apiClient._eventBus.on( 'calendarFetched', ( data ) => {
-            console.log('calendarFetch event received with data:', data );
-        });
-        */
-       const localeLabelAfter = document.querySelector('#localeLabelAfter');
-       const acceptLabelAfter = document.querySelector('#acceptLabelAfter');
-       const yearLabelAfter = document.querySelector('#yearLabelAfter');
-       apiOptions._localeInput._labelElement.insertAdjacentElement('beforeend', localeLabelAfter);
-       apiOptions._acceptHeaderInput._labelElement.insertAdjacentElement('beforeend', acceptLabelAfter);
-       apiOptions._yearInput._labelElement.insertAdjacentElement('beforeend', yearLabelAfter);
+        const localeLabelAfter = document.querySelector('#localeLabelAfter');
+        const acceptLabelAfter = document.querySelector('#acceptLabelAfter');
+        const yearLabelAfter = document.querySelector('#yearLabelAfter');
+        apiOptions._localeInput._labelElement.insertAdjacentElement('beforeend', localeLabelAfter);
+        apiOptions._acceptHeaderInput._labelElement.insertAdjacentElement('beforeend', acceptLabelAfter);
+        apiOptions._yearInput._labelElement.insertAdjacentElement('beforeend', yearLabelAfter);
+
+        const pathBuilder = new PathBuilder(apiOptions, calendarSelect)
+        .class('row align-items-center ps-2')
+        .pathWrapperClass('col-sm-8 border border-secondary rounded bg-light px-3 py-1')
+        .buttonWrapperClass('col-sm-3')
+        .buttonClass('btn btn-primary')
+        .replace('#pathBuilderComponent');
+
     }
 });
 
 
-(function ($) {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
-
-    $(document).on('change', '#APICalendarRouteSelect', function() {
-        RequestPayload.locale              = null;
-        RequestPayload.ascension           = null;
-        RequestPayload.corpus_christi      = null;
-        RequestPayload.epiphany            = null;
-        RequestPayload.year_type           = null;
-        RequestPayload.eternal_high_priest = null;
-        const selectEl = document.querySelector('#APICalendarSelect');
-        switch (this.value) {
-            case '/calendar':
-                CurrentEndpoint.calendarType       = null;
-                CurrentEndpoint.calendarId         = null;
-                break;
-            case '/calendar/nation/':
-                if ( CurrentEndpoint.calendarType !== CalendarType.NATIONAL ) {
-                    CurrentEndpoint.calendarId   = encodeURIComponent(selectEl.value);
-                    CurrentEndpoint.calendarType = CalendarType.NATIONAL;
-                }
-                break;
-            case '/calendar/diocese/':
-                if ( CurrentEndpoint.calendarType !== CalendarType.DIOCESAN ) {
-                    CurrentEndpoint.calendarId   = encodeURIComponent(selectEl.value);
-                    CurrentEndpoint.calendarType = CalendarType.DIOCESAN;
-                }
-                break;
-        }
-        $('#RequestURLExample').text(CurrentEndpoint.serialize());
-        $('#RequestURLButton').attr('href', CurrentEndpoint.serialize());
-    });
-
-    $(document).on('change', '#APICalendarSelect', function() {
-        const calendarType = $(this).find(':selected').attr("data-calendartype");
-        switch (calendarType){
-            case 'national':
-                CurrentEndpoint.calendarType = CalendarType.NATIONAL;
-                CurrentEndpoint.calendarId   = this.value;
-                break;
-            case 'diocesan': {
-                CurrentEndpoint.calendarType = CalendarType.DIOCESAN;
-                CurrentEndpoint.calendarId   = this.value;
-                break;
-            }
-        }
-        $('#RequestURLExample').text(CurrentEndpoint.serialize());
-        $('#RequestURLButton').attr('href', CurrentEndpoint.serialize());
-    });
-
-    $(document).on('change', '#RequestOptionReturnType', function() {
-        RequestPayload.return_type = this.value;
-        $('#RequestURLExample').text(CurrentEndpoint.serialize());
-        $('#RequestURLButton').attr('href', CurrentEndpoint.serialize());
-    });
-
-    $(document).on('change', '#RequestOptionYearType', function() {
-        RequestPayload.year_type = this.value;
-        $('#RequestURLExample').text(CurrentEndpoint.serialize());
-        $('#RequestURLButton').attr('href', CurrentEndpoint.serialize());
-    });
-
-    $(document).on( 'change', '.requestOption', function() {
-        switch($(this).attr("id")){
-            case 'RequestOptionEpiphany':
-                RequestPayload.epiphany = this.value;
-                break;
-            case 'RequestOptionCorpusChristi':
-                RequestPayload.corpus_christi = this.value;
-                break;
-            case 'RequestOptionAscension':
-                RequestPayload.ascension = this.value;
-                break;
-            case 'RequestOptionLocale':
-                RequestPayload.locale = this.value;
-                break;
-            case 'RequestOptionEternalHighPriest':
-                RequestPayload.eternal_high_priest = this.value;
-        }
-        $('#RequestURLExample').text(CurrentEndpoint.serialize());
-        $('#RequestURLButton').attr('href', CurrentEndpoint.serialize());
-    });
-
-    $(document).on( 'change', '#RequestOptionYear', function() {
-        CurrentEndpoint.calendarYear = this.value;
-        $('#RequestURLExample').text(CurrentEndpoint.serialize());
-        $('#RequestURLButton').attr('href', CurrentEndpoint.serialize());
-    });
-
-})(jQuery);
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
