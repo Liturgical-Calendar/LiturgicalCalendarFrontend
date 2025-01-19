33795fc486f04c4d05fbb7c4e8abe562368c1784
John R. D'Orazio <priest@johnromanodorazio.com>
2025-01-19 12:06:28 +0100

    we actually don't need a payload for DELETE requests

--- assets/js/DiocesanCalendarPayload.js
+++ assets/js/DiocesanCalendarPayload.js
@@ -40,51 +40,7 @@ class DiocesanCalendarPayload {
     }
 }
 
-/**
- * @typedef DiocesanCalendarDELETEPayload
- * @prop {string} diocese
- * @prop {string} nation
- */
-
-class DiocesanCalendarDELETEPayload {
-    /**
-     * Create a new DiocesanCalendarDELETEPaylod.
-     * @param {string} diocese - Diocese code.
-     * @param {string} nation - Nation code.
-     * @returns {DiocesanCalendarDELETEPayload} A new instance of DiocesanCalendarDELETEPayload.
-     */
-    constructor( diocese, nation ) {
-        const allowedProps = new Set(['diocese', 'nation']);
-        this.diocese = diocese;
-        this.nation = nation;
-
-        return new Proxy( this, {
-            get: (target, prop) => {
-                // Allow JSON.stringify
-                if (prop === 'toJSON') {
-                    return () => ({
-                        diocese: this.diocese,
-                        nation: this.nation
-                    });
-                }
-                if ( allowedProps.has( prop ) ) {
-                    return Reflect.get(target, prop);
-                }
-                throw new Error( `Cannot access invalid property "${prop}".` );
-            },
-
-            set( target, prop, value ) {
-                if ( allowedProps.has( prop ) ) {
-                    //TODO: Add validation for diocese and nation
-                    return Reflect.set(target, prop, value);
-                }
-                throw new Error( `Cannot add new property "${prop}".` );
-            }
-        } );
-    }
-}
 
 export {
-    DiocesanCalendarPayload,
-    DiocesanCalendarDELETEPayload
+    DiocesanCalendarPayload
 }
