3d62178d17c9eef0dc6b826bad37801592da2bf0
John R. D'Orazio <priest@johnromanodorazio.com>
2025-01-19 03:21:36 +0100

    fix JSON.stringify on `DiocesanCalendarDELETEPayload`

--- assets/js/DiocesanCalendarPayload.js
+++ assets/js/DiocesanCalendarPayload.js
@@ -15,20 +15,26 @@ class DiocesanCalendarPayload {
         this.metadata = metadata;
         return new Proxy( this, {
             get: (target, prop) => {
+                // Allow JSON.stringify
+                if (prop === 'toJSON') {
+                    return () => ({
+                        litcal: this.litcal,
+                        settings: this.settings,
+                        metadata: this.metadata
+                    });
+                }
                 if ( allowedProps.has( prop ) ) {
                     return Reflect.get(target, prop);
-                } else {
-                    throw new Error( `Cannot access invalid property "${prop}".` );
                 }
+                throw new Error( `Cannot access invalid property "${prop}".` );
             },
 
             set( target, prop, value ) {
                 if ( allowedProps.has( prop ) ) {
                     //TODO: Add validations
                     return Reflect.set(target, prop, value);
-                } else {
-                    throw new Error( `Cannot add new property "${prop}".` );
                 }
+                throw new Error( `Cannot add new property "${prop}".` );
             }
         } );
     }
@@ -54,26 +60,25 @@ class DiocesanCalendarDELETEPayload {
 
         return new Proxy( this, {
             get: (target, prop) => {
-                if ( allowedProps.has( prop ) || (prop === 'toJSON' && typeof target[prop] === 'function') ) {
-                    if (prop === 'toJSON') {
-                        return () => ({
-                            diocese: this.diocese,
-                            nation: this.nation
-                        });
-                    }
-                    return Reflect.get(target, prop);
-                } else {
-                    throw new Error( `Cannot access invalid property "${prop}".` );
+                // Allow JSON.stringify
+                if (prop === 'toJSON') {
+                    return () => ({
+                        diocese: this.diocese,
+                        nation: this.nation
+                    });
                 }
+                if ( allowedProps.has( prop ) ) {
+                    return Reflect.get(target, prop);
+                }
+                throw new Error( `Cannot access invalid property "${prop}".` );
             },
 
             set( target, prop, value ) {
                 if ( allowedProps.has( prop ) ) {
                     //TODO: Add validation for diocese and nation
                     return Reflect.set(target, prop, value);
-                } else {
-                    throw new Error( `Cannot add new property "${prop}".` );
                 }
+                throw new Error( `Cannot add new property "${prop}".` );
             }
         } );
     }
